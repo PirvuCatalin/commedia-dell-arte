6e61ed78a51144871e809b82fe591c9212433ef3
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-11-01 05:43:14 +0200

    added sliders and fixed map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { CommonService } from './service/common.service';
 import { NewsfeedService } from './service/newsfeed.service';
 import { YearIntervals } from './model/yearIntervals';
 import { CountryTemperature } from './model/countryTemperature';
+import { Options as SliderOptions, ChangeContext} from '@angular-slider/ngx-slider';
 
 @Component({
   selector: "my-app",
@@ -17,17 +18,74 @@ import { CountryTemperature } from './model/countryTemperature';
 export class AppComponent implements OnInit  {
   constructor(private globals: Globals, private commonService: CommonService, private newsfeedService: NewsfeedService) { }
   
+  value: number = this.globals.year;
+  yearOptions: SliderOptions = {
+    floor: 0,
+    ceil: 250
+  };
+
+  months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov", "Dec"]
+
+  monthOptions: SliderOptions = {
+    floor: 1,
+    ceil: 12,
+    showTicks: true,
+    getLegend: (value: number): string => {
+      return this.months[value-1];
+    }
+  };
+
+  changeYearOptions(minVal, maxVal) {
+    const newOptions: SliderOptions = Object.assign({}, this.yearOptions);
+    newOptions.floor = minVal;
+    newOptions.ceil = maxVal;
+    this.yearOptions = newOptions;
+  }
+
+  config = {
+    value: true,
+    name: '',
+    disabled: false,
+    height: 25,
+    width: 50,
+    margin: 3,
+    fontSize: 10,
+    speed: 300,
+    color: {
+      checked: '#56C128',
+      unchecked: '#dcdcdc',
+    },
+    switchColor: {
+      checked: '#3366FF',
+      unchecked: 'crimson'
+    },
+    labels: {
+      unchecked: 'off',
+      checked: 'on',
+    },
+    checkedLabel: '',
+    uncheckedLabel: '',
+    fontColor: {
+      checked: '#fafafa',
+      unchecked: '#ffffff'
+    }
+  };
+
   yearIntervals : YearIntervals = {
     yearMax: null,
     yearMin: null
   };
   
   updateTemperaturesFlag = false;
+  
   countryTemperatures = [];
+
 
   ngOnInit(): void {
     this.newsfeedService.getYearIntervals().subscribe(yearIntervals => {
       this.yearIntervals = yearIntervals;
+      this.changeYearOptions(this.yearIntervals.yearMin,this.yearIntervals.yearMax);
+      this.globals.year = 2003;
     });
 
     this.updateTemperatures();
@@ -44,10 +102,15 @@ export class AppComponent implements OnInit  {
     });
   }
 
-  sliderOnChange(value: number) {
-    this.globals.year = value;
+  yearSliderOnChange(changeContext : ChangeContext) {
+    this.globals.year = changeContext.value;
     this.updateTemperatures();
-    this.commonService.sliderChanged(value);
+    this.commonService.sliderChanged(changeContext.value);
+  }
+
+  monthSliderOnChange(changeContext : ChangeContext) {
+    this.globals.month = changeContext.value;
+    this.updateTemperatures();
   }
   
   Highcharts: typeof Highcharts = Highcharts;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HighchartsChartModule } from 'highcharts-angular';
@@ -13,9 +13,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatCardModule} from '@angular/material/card';
+import { NgToggleModule } from 'ng-toggle-button';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HighchartsChartModule, MDBBootstrapModule.forRoot() , HttpClientModule, BrowserAnimationsModule, MatSliderModule, MatCardModule],
+  imports:      [ BrowserModule, FormsModule, HighchartsChartModule, MDBBootstrapModule.forRoot() , HttpClientModule, BrowserAnimationsModule, MatSliderModule, MatCardModule, NgToggleModule, NgxSliderModule, ReactiveFormsModule],
   declarations: [ AppComponent, BrandComponent, FooterComponent, NewsfeedComponent ],
   bootstrap:    [ AppComponent ],
   providers: [Globals]
